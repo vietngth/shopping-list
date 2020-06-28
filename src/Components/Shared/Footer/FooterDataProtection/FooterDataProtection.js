da08c738104444becf6d2ceb585043ba052167b6
Viet Nguyen <vietnguyenthe97@gmail.com>
2020-06-28 22:27:56 +0700

    Add FooterDataProtection component

---
+++ src/Components/Shared/Footer/FooterDataProtection/FooterDataProtection.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './FooterDataProtection.scss';
+
+const FooterDataProtection = () => {
+      return (
+            <>
+                  <a className="footer__protection-item" href="/">
+                        Privacy Policy
+                  </a>
+                  <a className="footer__protection-item" href="/">
+                        Terms &amp; Conditions
+                  </a>
+            </>
+      )
+}
+
+export default FooterDataProtection;
